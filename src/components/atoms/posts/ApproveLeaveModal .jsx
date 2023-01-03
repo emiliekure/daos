9f93669d29bad6bc1bb20b4999211ee8460971c6
Sandra Krejcir <krejcir.sandra@gmail.com>
2023-01-03 14:39:44 +0100

    modal and settings styling changes

--- src/components/atoms/posts/ApproveLeaveModal .jsx
+++ src/components/atoms/posts/ApproveLeaveModal .jsx
@@ -3,45 +3,41 @@ import SecondaryButton from "../buttons/SecondaryButton";
 import styles from "./PostItem.module.css";
 import btnstyles from "../../shared/TheHeader.module.css";
 
-export default function ApproveLeaveModal({
+export default function UnauthorisedModal({
   style,
   setApproveLeave,
   handleRemoveMember,
   id,
 }) {
   return (
-    <div className={style} id="approveLeaveModal">
-      <div className={styles.contentWrapper}>
-        <div className={styles.postContent}>
+    <div className={style} id="unauthorisedModal">
+      <div className={styles.modalWrapper}>
+        <div className={styles.postHeader}>
+          <p style={{ marginTop: 10 }}>
+            Are you sure you would like to leave this group?
+          </p>
           <button
             type="button"
             name="closeBtn"
             id="closeBtn"
+            className={styles.closeBtn}
             onClick={setApproveLeave}
           >
-            X
+            <img src="../img/xmark-solid.svg" alt="close icon" />
           </button>
-          <h4 className={styles.postTitle}>
-            Are you sure you want to leave this ensemble?
-          </h4>
-          <p>
-            By clicking leave, you will no longer be a part of this ensemble.
-            However, you can easily rejoin the group later, if there are free
-            member places available.
-          </p>
-          <div className={btnstyles.buttons}>
-            <SecondaryButton
-              type="button"
-              text="Cancel"
-              onClick={setApproveLeave}
-            />
-            <PrimaryButton
-              id={id}
-              type="button"
-              text="Leave"
-              onClick={(event) => handleRemoveMember(event.target.id)}
-            />
-          </div>
+        </div>
+        <div className={btnstyles.buttons}>
+          <SecondaryButton
+            type="button"
+            text="Cancel"
+            onClick={setApproveLeave}
+          />
+          <PrimaryButton
+            id={id}
+            type="button"
+            text="Leave"
+            onClick={(event) => handleRemoveMember(event.target.id)}
+          />
         </div>
       </div>
     </div>
